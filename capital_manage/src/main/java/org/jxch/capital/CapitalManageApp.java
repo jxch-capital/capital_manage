d29be66415938494c0fe3e3305c78bf85d34311f
jxch <15553598935@163.com>
2023-04-04 09:19:28 +0800

    init

---
+++ capital_manage/src/main/java/org/jxch/capital/CapitalManageApp.java
@@ -0,0 +1,11 @@
+package org.jxch.capital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalManageApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalManageApp.class, args);
+    }
+}
